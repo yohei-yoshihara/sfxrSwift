5accd65e5bd289897dc4a09ee3cac0d99dd12bbb
Yohei Yoshihara <yy2.0101@gmail.com>
2017-01-23 20:07:13 +0900

    Initial Commit

---
+++ sfxrSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sfxrSwift
+//
+//  Created by 吉原 洋平 on 2017/01/23.
+//  Copyright © 2017年 Yohei Yoshihara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ sfxrSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sfxrSwift
+//
+//  Created by 吉原 洋平 on 2017/01/23.
+//  Copyright © 2017年 Yohei Yoshihara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
